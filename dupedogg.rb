b2e976224a63cdd2a52ab237a751d2c627381907
Francesco Mari <francescojjmari@gmail.com>
2025-07-20 16:17:46 +0200

    Fix formula with correct URLs and Language::Python::Virtualenv

--- dupedogg.rb
+++ dupedogg.rb
@@ -1,4 +1,6 @@
 class Dupedogg < Formula
+  include Language::Python::Virtualenv
+
   desc "A command-line tool to find and move similar images"
   homepage "https://github.com/sekmo/dupedogg"
   url "https://github.com/sekmo/dupedogg/archive/refs/tags/v0.1.0.tar.gz"
@@ -8,18 +10,18 @@ class Dupedogg < Formula
   depends_on "python@3.12"
 
   resource "Pillow" do
-    url "https://files.pythonhosted.org/packages/cd/74/ad3d526f3bf7b6d3f408b73fde271ec69dfac8b81341a318ce825f2b3812/pillow-10.4.0.tar.gz"
-    sha256 "166c1cd4d24309b30d61f79f4a9114b7b2313d7450912277855ff5dfd7cd4a06"
+    url "https://files.pythonhosted.org/packages/f3/0d/d0d6dea55cd152ce3d6767bb38a8fc10e33796ba4ba210cbab9354b6d238/pillow-11.3.0.tar.gz"
+    sha256 "3828ee7586cd0b2091b6209e5ad53e20d0649bbe87164a459d0676e035e8f523"
   end
 
   resource "ImageHash" do
-    url "https://files.pythonhosted.org/packages/c6/bb/1f300a41d7c4eb2aca1b6de8d4e66d7e3a2e65d1170a8e5b49ed8b420b30/ImageHash-4.3.1.tar.gz"
-    sha256 "7038d1b975a34ddc09500a32f80cdc17edc3d79a29bb6c58a12ca332a0ed4b1e"
+    url "https://files.pythonhosted.org/packages/cd/de/5c0189b0582e21583c2a213081c35a2501c0f9e51f21f6a52f55fbb9a4ff/ImageHash-4.3.2.tar.gz"
+    sha256 "e54a79805afb82a34acde4746a16540503a9636fd1ffb31d8e099b29bbbf8156"
   end
 
   resource "tqdm" do
-    url "https://files.pythonhosted.org/packages/58/83/6ba9844a41128c62e810fddddd72473201f3eacde02046066142a2d96cc5b/tqdm-4.66.5.tar.gz"
-    sha256 "e1020aef2e5096702d8a025ac7d16b1577279c9d63f8375b63083e9a5f0fcbad"
+    url "https://files.pythonhosted.org/packages/a8/4b/29b4ef32e036bb34e4ab51796dd745cdba7ed47ad142a9f4a1eb8e0c744d/tqdm-4.67.1.tar.gz"
+    sha256 "f8aef9c52c08c13a65f30ea34f4e5aac3fd1a34959879d7e59e63027286627f2"
   end
 
   def install
