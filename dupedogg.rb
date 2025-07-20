84bf485d515dd27616657f5ea57ada279cb09faa
Francesco Mari <francescojjmari@gmail.com>
2025-07-20 16:39:59 +0200

    Simplify formula - let pip handle ImageHash dependencies automatically

--- dupedogg.rb
+++ dupedogg.rb
@@ -30,37 +30,8 @@ class Dupedogg < Formula
     sha256 "f8aef9c52c08c13a65f30ea34f4e5aac3fd1a34959879d7e59e63027286627f2"
   end
 
-  resource "numpy" do
-    url "https://files.pythonhosted.org/packages/2e/19/d7c972dfe90a353dbd3efbbe1d14a5951de80c99c9dc1b93cd998d51dc0f/numpy-2.3.1.tar.gz"
-    sha256 "1ec9ae20a4226da374362cca3c62cd753faf2f951440b0e3b98e93c235441d2b"
-  end
-
-  resource "PyWavelets" do
-    url "https://files.pythonhosted.org/packages/48/45/bfaaab38545a33a9f06c61211fc3bea2e23e8a8e00fedeb8e57feda722ff/pywavelets-1.8.0.tar.gz"
-    sha256 "f3800245754840adc143cbc29534a1b8fc4b8cff6e9d403326bd52b7bb5c35aa"
-  end
-
-  resource "scipy" do
-    url "https://files.pythonhosted.org/packages/81/18/b06a83f0c5ee8cddbde5e3f3d0bb9b702abfa5136ef6d4620ff67df7eee5/scipy-1.16.0.tar.gz"
-    sha256 "b5ef54021e832869c8cfb03bc3bf20366cbcd426e02a58e8a58d7584dfbb8f62"
-  end
-
   def install
-    virtualenv_create(libexec, "python3.12")
-    
-    # Install resources
-    resource("Pillow").stage { system libexec/"bin/python", "-m", "pip", "install", "." }
-    resource("ImageHash").stage { system libexec/"bin/python", "-m", "pip", "install", "." }
-    resource("tqdm").stage { system libexec/"bin/python", "-m", "pip", "install", "." }
-    resource("numpy").stage { system libexec/"bin/python", "-m", "pip", "install", "." }
-    resource("PyWavelets").stage { system libexec/"bin/python", "-m", "pip", "install", "." }
-    resource("scipy").stage { system libexec/"bin/python", "-m", "pip", "install", "." }
-    
-    # Install the main package
-    system libexec/"bin/python", "-m", "pip", "install", "."
-    
-    # Create binary symlink
-    bin.install_symlink libexec/"bin/dupedogg"
+    virtualenv_install_with_resources
   end
 
   test do
