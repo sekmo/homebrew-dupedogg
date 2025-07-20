d7cd11d31623ce7c9099467fab266b5f5f0587c4
Francesco Mari <francescojjmari@gmail.com>
2025-07-20 16:42:12 +0200

    Allow ImageHash to install its own dependencies (numpy, scipy, PyWavelets)

--- dupedogg.rb
+++ dupedogg.rb
@@ -20,18 +20,30 @@ class Dupedogg < Formula
     sha256 "3828ee7586cd0b2091b6209e5ad53e20d0649bbe87164a459d0676e035e8f523"
   end
 
-  resource "ImageHash" do
-    url "https://files.pythonhosted.org/packages/cd/de/5c0189b0582e21583c2a213081c35a2501c0f9e51f21f6a52f55fbb9a4ff/ImageHash-4.3.2.tar.gz"
-    sha256 "e54a79805afb82a34acde4746a16540503a9636fd1ffb31d8e099b29bbbf8156"
-  end
-
   resource "tqdm" do
     url "https://files.pythonhosted.org/packages/a8/4b/29b4ef32e036bb34e4ab51796dd745cdba7ed47ad142a9f4a1eb8e0c744d/tqdm-4.67.1.tar.gz"
     sha256 "f8aef9c52c08c13a65f30ea34f4e5aac3fd1a34959879d7e59e63027286627f2"
   end
 
   def install
-    virtualenv_install_with_resources
+    virtualenv_create(libexec, "python3.12")
+    
+    # Install Pillow and tqdm without dependencies
+    resource("Pillow").stage do
+      system libexec/"bin/python", "-m", "pip", "install", "--no-deps", "."
+    end
+    resource("tqdm").stage do
+      system libexec/"bin/python", "-m", "pip", "install", "--no-deps", "."
+    end
+    
+    # Install ImageHash with dependencies (allows numpy, scipy, PyWavelets to be installed)
+    system libexec/"bin/python", "-m", "pip", "install", "ImageHash==4.3.2"
+    
+    # Install the main package
+    system libexec/"bin/python", "-m", "pip", "install", "--no-deps", "."
+    
+    # Create binary symlink
+    bin.install_symlink libexec/"bin/dupedogg"
   end
 
   test do
